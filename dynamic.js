c0b532629551b879056f39dc21f5604fc7e15a87
Alexander <isitinme88@gmail.com>
2015-05-05 13:22:33 +0300

    0.2.0

--- dynamic.js
+++ dynamic.js
@@ -7,7 +7,7 @@ function DR( elem ) {
 	}
 }
 
-DR.prototype.exec = function( prop, normal ) {
+DR.prototype.exec = function( prop, normal, breakpoint ) {
 	var selfElem = this.elem;
 	
 	var defElemValue = parseInt( this.styles()[prop] ),
@@ -15,21 +15,50 @@ DR.prototype.exec = function( prop, normal ) {
 
 	if( normal ) {
 		defaultWindowWidth = normal;
-		set( defaultWindowWidth );
+		set( defaultWindowWidth, breakpoint );
 	} else {
 		defaultWindowWidth = document.documentElement.clientWidth;
-		set( defaultWindowWidth );
+		set( defaultWindowWidth, breakpoint );
 	}
 	
-	function set( def ) {
+	function set( def, breakpoint ) {
 		var currentWindowWidth = document.documentElement.clientWidth;
-		var perc = currentWindowWidth * 100 / def;
-		selfElem.style[prop] = defElemValue * perc / 100 + 'px';
+		
+		if( breakpoint && ( currentWindowWidth <= breakpoint.point ) ) {
+			applyBreakPoint( currentWindowWidth );
+			return;
+		} else {
+			var perc = currentWindowWidth * 100 / def;
+			selfElem.style[prop] = defElemValue * perc / 100 + 'px';
+		}
+	
 	}
 	
 	window.addEventListener('resize', function() {
 		var newWindowWidth = document.documentElement.clientWidth;
+		
+		if( breakpoint ) {
+			if( newWindowWidth <= breakpoint.point ) {
+				applyBreakPoint( newWindowWidth );
+				return;
+			} else {
+				setResize( newWindowWidth );
+			}
+		}
+		
 		var newPerc = newWindowWidth * 100 / defaultWindowWidth;
 		selfElem.style[prop] = defElemValue * newPerc / 100 + 'px';
 	});
+	
+	function setResize( newWidth ) {
+		var perc = newWidth * 100 / defaultWindowWidth;
+		selfElem.style[prop] = defElemValue * perc / 100 + 'px';
+	}	
+	
+	function applyBreakPoint( newWidth ) {
+		var newElemValue = breakpoint.value;
+		var perc = newWidth * 100 / breakpoint.point;
+		selfElem.style[prop] = newElemValue * perc / 100 + 'px';
+	}
+	
 }				
